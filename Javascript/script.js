c7371ffd27f871d85052a71d134613066a524f1a
Daniel Obazee <obazeedaniel03@gmail.com>
2025-07-30 16:13:20 -0700

    Implemented main game logic for guess checking and feedback. Added event listener for the "Check!" button to handle user guesses, update score, and display feedback messages for correct, too high, too low, and invalid inputs. Sets up the core interactive loop for the 90's inspired number guessing game.

--- Javascript/script.js
+++ Javascript/script.js
@@ -6,11 +6,35 @@
 // document.querySelector(".guess").value = 23;
 // console.log(document.querySelector(".guess").value);
 
+const secretNumber = Math.ceil(Math.random() * 20);
+let score = 5;
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = "⛔️ No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score < 1) {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+      return;
+    }
+    document.querySelector(".message").textContent = "📉 Too low!";
+    score--;
+    document.querySelector(".score").textContent = score;
   }
 });
